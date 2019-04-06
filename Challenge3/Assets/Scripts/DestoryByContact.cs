821a453dc3aef26ced63f641d8015cae51fb171a
Greta-Lundy <Greta.m.lundy@gmail.com>
2019-04-06 12:43:55 -0400

    Challenge3

---
+++ Challenge3/Assets/Scripts/DestoryByContact.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestoryByContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerexplosion;
+    public int scoreValue;
+    private GameController GC;
+
+    private void Start()
+    {
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if(gameControllerObject != null)
+        {
+            GC = gameControllerObject.GetComponent<GameController>();
+        }
+        if(GC == null)
+        {
+            Debug.Log("Cannot find 'GameController' script");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Boundary") || other.CompareTag("Enemy"))
+        {
+            return;
+        }
+        if (explosion != null)
+        {
+            Instantiate(explosion, transform.position, transform.rotation);
+        }
+
+        if(other.CompareTag("Player"))
+        {
+            Instantiate(playerexplosion, other.transform.position, other.transform.rotation);
+            GC.GameOver();
+        }
+        GC.AddScore(scoreValue);
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
---
+++ Challenge3/Assets/Scripts/GameController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValue;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text ScoreText;
+    public Text restartText;
+    public Text gameOverText;
+    public Text creditText;
+    private int score;
+
+    private bool gameOver;
+    private bool restart;
+
+    private void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        creditText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine (SpawnWaves());
+    }
+
+    private void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.B))
+            {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+        if (Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0,hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValue.x, spawnValue.x), spawnValue.y, spawnValue.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+            if (gameOver)
+            {
+                restartText.text = "Press 'B' to restart";
+                restart = true;
+                break;
+            }
+        }
+        
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+    void UpdateScore()
+    {
+        ScoreText.text = "Points: " + score;
+        if(score >= 100)
+        {
+            gameOverText.text = "You win!";
+            creditText.text = "Game created by Greta Lundy";
+            gameOver = true;
+            restart = true;
+        }
+    }
+}
+ 
